26baa5aff568e0a973e4a2bfee7c25b9fe11c1ae
Omokefe Ovie David <ovie.omokefe@premiumtrustbank.com>
2024-10-06 19:29:05 +0100

    initial setUp and restructure

---
+++ utils/tailwindView.ts
@@ -0,0 +1,6 @@
+import { styled } from "nativewind"
+import { Text, View } from "react-native"
+
+
+export const StyledView = styled(View)   // Wrap View in nativewind styled
+export const StyledText = styled(Text) 
